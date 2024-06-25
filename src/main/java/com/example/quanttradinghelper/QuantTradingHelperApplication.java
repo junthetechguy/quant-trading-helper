158162ffb75e043b996aff1e075a85455b178cef
junthetechguy <forestwoodleaves@gmail.com>
2024-06-25 16:55:32 +0900

    init: Initialize project

---
+++ src/main/java/com/example/quanttradinghelper/QuantTradingHelperApplication.java
@@ -0,0 +1,14 @@
+package com.example.quanttradinghelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class QuantTradingHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantTradingHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quanttradinghelper/QuantTradingHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanttradinghelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantTradingHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
